405890a6360d565b692fc4a97ad7b0e61bd71ad4
Alexander Y <startyourday60@gmail.com>
2025-08-17 10:09:44 +0000

    fix: heat map fix

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -49,23 +49,25 @@ def draw_cat_plot():
 def draw_heat_map():
     # 11
     global df_heat
-    df_heat = df.loc[(df['ap_lo'] <= df['ap_hi'])  & (df['height'] >= df['height'].quantile(0.025))  & (df['height']>97.5) &(df['weight']<2.5) &(df['weight']>97.5)]
+    df_heat = df.loc[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = df.corr()
+    corr = df_heat.corr()
 
     # 13 by chatgpt because I not clear what there need. mask not tested
     mask = np.triu(np.ones_like(corr, dtype=bool))
-    
-
-
 
     # 14
     fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
     sns.heatmap(corr, mask=mask, cmap="coolwarm", annot=True, fmt=".1f", square=True, center=0)
-
 
     # 16
     fig.savefig('heatmap.png')
